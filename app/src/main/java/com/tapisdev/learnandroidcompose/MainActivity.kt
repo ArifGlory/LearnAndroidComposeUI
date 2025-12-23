a148dd4a4869d59c637f468e982a9d6ec400e36e
Arif Glory <arifglory46@gmail.com>
2025-12-23 11:03:06 +0700

    kedua

--- app/src/main/java/com/tapisdev/learnandroidcompose/MainActivity.kt
+++ app/src/main/java/com/tapisdev/learnandroidcompose/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.tapisdev.learnandroidcompose
 
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,20 +15,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.VectorProperty
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tapisdev.learnandroidcompose.ui.theme.LearnAndroidComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,15 +44,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnAndroidComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "memew",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    UserProfile()
                 }
             }
         }
     }
+
+    fun showToast(){
+        Toast.makeText(this@MainActivity,"Showing toast", Toast.LENGTH_SHORT).show()
+    }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -54,20 +67,35 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun UserProfile() {
-    Box(modifier = Modifier) {
-        Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "hihihi",
-            modifier = Modifier.size(32.dp)
-        )
-        Icon(
-            imageVector = Icons.Default.Check,
-            contentDescription = null,
-            modifier = Modifier
-                .size(8.dp)
-                .align(Alignment.BottomEnd)
-                .padding(1.dp)
-        )
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.padding(10.dp)) {
+        Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "hihihi",
+                modifier = Modifier.size(32.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(8.dp)
+                    .align(Alignment.BottomEnd)
+                    .padding(1.dp)
+            )
+        }
+        Text("Nama")
+        Button(onClick = {
+            Toast.makeText(context,"Showing toast", Toast.LENGTH_SHORT).show()
+        }) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Add")
+        }
     }
 }
 
